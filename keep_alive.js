60ac878fb20eeb451a127fd84b2c3b25c7500734
g4bplayer83-glitch <g4bplayer83@gmail.com>
2026-05-05 20:40:40 +0200

    Update keep_alive.js

--- keep_alive.js
+++ keep_alive.js
@@ -1,14 +1,15 @@
 const express = require('express');
 const app = express();
 
-app.all('/', (req, res) => {
+app.get('/', (req, res) => {
     res.send('Mon bot est en ligne et écoute !');
 });
 
 function keepAlive() {
-    // Le port 3000 est un standard pour les hébergeurs
-    app.listen(3000, () => {
-        console.log("Serveur web prêt pour le 24/7.");
+    // Render utilise la variable process.env.PORT, il faut lui dire de l'écouter
+    const port = process.env.PORT || 3000;
+    app.listen(port, () => {
+        console.log(`Serveur web prêt sur le port ${port}`);
     });
 }
 
